986268baf44bdbb534599fc4fb79ddfaac048663
sayedsyfuzzaman <sayedsyfuzzaman@gmail.com>
2022-06-06 00:58:13 +0600

    Layout, Routes

--- TASK_1/routes/web.php
+++ TASK_1/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/',[PagesController::class, 'home'])->name('home');
+Route::get('/services',[PagesController::class, 'services'])->name('services');
